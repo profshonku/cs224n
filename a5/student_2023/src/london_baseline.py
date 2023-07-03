6f24bc87d5ae762934dcff2a9465b3e57eac629d
Soumitra Das <soumitradas00002@gmail.com>
2023-07-03 11:06:03 +0530

    london_baseline done

--- a5/student_2023/src/london_baseline.py
+++ a5/student_2023/src/london_baseline.py
@@ -2,3 +2,13 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+
+import utils
+eval_corpus_path = 'birth_dev.tsv'
+predictions = []
+for line in open(eval_corpus_path, encoding='utf-8'):
+    pred = "London"
+    predictions.append(pred)
+total, correct = utils.evaluate_places(eval_corpus_path, predictions)
+print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
+    
